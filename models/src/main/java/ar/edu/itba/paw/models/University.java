69914c2834e82308722a42368bc0e442d4a3a7c4
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-27 18:15:26 -0300

    Fix university model

--- models/src/main/java/ar/edu/itba/paw/models/University.java
+++ models/src/main/java/ar/edu/itba/paw/models/University.java
@@ -1,17 +1,26 @@
 package ar.edu.itba.paw.models;
 
 public class University{
+    private final long id;
     private final String name;
     private final String abbreviation;
+
+    public University(long id, String name, String abbreviation){
+        this.id = id;
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
 
     public University(String name, String abbreviation){
         this.name = name;
         this.abbreviation = abbreviation;
+        this.id = 0; // FIXME
     }
 
     public University(String name){
         this.name = name;
         this.abbreviation = null;
+        this.id = 0; // FIXME
     }
 
     public String getName() {
