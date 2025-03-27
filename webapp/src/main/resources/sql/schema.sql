64b82143a9f3e52fbf92940b9cd3570ea89ff8dd
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-27 16:27:25 -0300

    Add DataSourceInitializer and preliminary schema.sql

---
+++ webapp/src/main/resources/sql/schema.sql
@@ -0,0 +1,118 @@
+-- CREATE TABLE IF NOT EXISTS image (
+--         id SERIAL PRIMARY KEY,
+--         content BYTEA NOT NULL
+-- );
+
+-- CREATE TABLE IF NOT EXISTS category (
+--         id SERIAL PRIMARY KEY,
+--         name varchar(100) NOT NULL UNIQUE
+-- );
+
+-- CREATE TABLE IF NOT EXISTS user_interest (
+--         user_id INTEGER NOT NULL,
+--         category_id INTEGER NOT NULL,
+--         description VARCHAR(200),
+--         PRIMARY KEY (user_id, category_id),
+--         FOREIGN KEY (category_id) REFERENCES category ON DELETE CASCADE
+-- );
+
+
+CREATE TABLE IF NOT EXISTS university (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        abbreviation VARCHAR(255) DEFAULT NULL
+);
+
+
+-- CREATE TABLE IF NOT EXISTS career (
+--         id SERIAL PRIMARY KEY,
+--         name VARCHAR(255) NOT NULL UNIQUE
+-- );
+
+-- CREATE TABLE IF NOT EXISTS career_in_university (
+--         id SERIAL PRIMARY KEY,
+--         career_id INTEGER NOT NULL,
+--         university_id INTEGER NOT NULL,
+--         FOREIGN KEY (career_id) REFERENCES career(id) ON DELETE CASCADE,
+--         FOREIGN KEY (university_id) REFERENCES university(id) ON DELETE CASCADE,
+--         UNIQUE (career_id, university_id)
+-- );
+
+
+CREATE TABLE IF NOT EXISTS "user" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        firstname VARCHAR(100) NOT NULL,
+        lastname VARCHAR(100) NOT NULL,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        university INTEGER NOT NULL,            -- to delete
+        career VARCHAR(50),                     -- career_in_university_id INTEGER NOT NULL,
+--         password VARCHAR(100) NOT NULL,
+--         profile_picture_id VARCHAR(1023) NOT NULL,
+
+        FOREIGN KEY (university) REFERENCES university(id) ON DELETE RESTRICT   -- to delete
+--         FOREIGN KEY (career_in_university_id) REFERENCES career_in_university ON DELETE RESTRICT,
+--         FOREIGN KEY (profile_picture_id) REFERENCES image ON DELETE RESTRICT
+
+);
+
+-- CREATE TABLE IF NOT EXISTS area_of_study (
+--         id SERIAL PRIMARY KEY,
+--         name VARCHAR(255) NOT NULL UNIQUE
+-- );
+
+-- CREATE TABLE IF NOT EXISTS career_area (
+--         id SERIAL PRIMARY KEY,
+--         career_id INTEGER NOT NULL,
+--         area_of_study_id INTEGER NOT NULL,
+--         UNIQUE(career_id, area_of_study_id),
+--         FOREIGN KEY (career_id) REFERENCES career(id) ON DELETE CASCADE,
+--         FOREIGN KEY (area_of_study_id) REFERENCES area_of_study(id) ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE IF NOT EXISTS city (
+-- id SERIAL PRIMARY KEY,
+-- name VARCHAR(100)
+-- );
+
+
+CREATE TABLE IF NOT EXISTS journey (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        destination_university_id INTEGER NOT NULL,
+        city VARCHAR(100) NOT NULL,                     -- city_id INTEGER NOT NULL,
+        start_date DATE NOT NULL,
+        end_date DATE NOT NULL,
+        description VARCHAR(2047),
+
+        FOREIGN KEY (destination_university_id) REFERENCES university ON DELETE RESTRICT,
+        FOREIGN KEY (user_id) REFERENCES "user" ON DELETE RESTRICT
+        -- FOREIGN KEY (city_id) REFERENCES city
+);
+
+
+-- CREATE TABLE IF NOT EXISTS interested_in_journey (
+--         user_id_from INTEGER NOT NULL,
+--         journey_id INTEGER NOT NULL,
+--         descripcion VARCHAR(1023) NOT NULL,
+--
+--         FOREIGN KEY (user_id_from) REFERENCES "user"(id) ON DELETE CASCADE,
+--         FOREIGN KEY (journey_id) REFERENCES journey(id) ON DELETE CASCADE,
+--         PRIMARY KEY (user_id_from, journey_id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS event (
+--         id SERIAL PRIMARY KEY,
+--         user_id INTEGER NOT NULL,
+--         destination_university_id INTEGER NOT NULL,
+--         city_id INTEGER NOT NULL,
+--         event_date DATE NOT NULL,
+--         description VARCHAR(2047),
+--         flyer_image_id INTEGER,
+
+--         FOREIGN KEY (destination_university_id) REFERENCES university ON DELETE RESTRICT,
+--         FOREIGN KEY user_id REFERENCES "user" ON DELETE RESTRICT,
+--         FOREIGN KEY (city_id) REFERENCES city,
+--         FOREIGN KEY (flyer_image_id) REFERENCES image
+-- );
