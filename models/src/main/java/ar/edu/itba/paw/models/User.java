b463ad59b142a5caa7d476e4a38768d756a9288d
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-27 10:43:59 -0300

    Change in user model

--- models/src/main/java/ar/edu/itba/paw/models/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -4,17 +4,17 @@ public class User{
     private final int id;
     private final String email;
     private final String username;
-    private final String password;
+    // private final String password;
     private final String firstname;
     private final String lastname;
     private final University university;
     private final int profilePictureId;
 
-    public User(int id, String email, String username, String password, String firstname, String lastname, University university, int profilePictureId){
+    public User(int id, String email, String username, /* String password, */ String firstname, String lastname, University university, int profilePictureId){
         this.id = id;
         this.email = email;
         this.username = username;
-        this.password = password;
+        // this.password = password;
         this.firstname = firstname;
         this.lastname = lastname;
         this.university = university;
@@ -33,9 +33,11 @@ public class User{
         return username;
     }
 
+    /*
     public String getPassword() {
         return password;
     }
+    */
 
     public String getFirstname() {
         return firstname;
