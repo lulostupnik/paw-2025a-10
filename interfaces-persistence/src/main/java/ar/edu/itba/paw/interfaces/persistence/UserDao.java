99c35080f2328d2b064efb1210d699a7977dcc65
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-27 18:34:36 -0300

    Adding profilePictureId to User

--- interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/UserDao.java
+++ interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/UserDao.java
@@ -5,7 +5,7 @@ import ar.edu.itba.paw.models.User;
 import java.util.Optional;
 
 public interface UserDao {
-    User create(String email, String username, String firstname, String lastname, long universityId, String career);
+    User create(String email, String username, String firstname, String lastname, long universityId, String career, long profilePictureId);
     Optional<User> findById(long id);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
