dbc810c176e13add8f73c6e268908ced96bf8628
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-29 23:28:58 -0300

    Fix names

--- interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/JourneyResponseDao.java
+++ interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/JourneyResponseDao.java
@@ -3,5 +3,5 @@ package ar.edu.itba.paw.interfaces.persistence;
 import ar.edu.itba.paw.models.JourneyResponse;
 
 public interface JourneyResponseDao {
-    JourneyResponse create(long user_id, long journey_id, String message);
+    JourneyResponse create(long userId, long journeyId, String message);
 }
--- models/src/main/java/ar/edu/itba/paw/models/JourneyResponse.java
+++ models/src/main/java/ar/edu/itba/paw/models/JourneyResponse.java
@@ -1,22 +1,22 @@
 package ar.edu.itba.paw.models;
 
 public class JourneyResponse {
-    private final long user_id;
-    private final long journey_id;
+    private final long userId;
+    private final long journeyId;
     private final String message;
 
-    public JourneyResponse(long user_id, long journey_id, String message){
-        this.user_id = user_id;
-        this.journey_id = journey_id;
+    public JourneyResponse(long userId, long journeyId, String message){
+        this.userId = userId;
+        this.journeyId = journeyId;
         this.message = message;
     }
 
     public long getUserId() {
-        return user_id;
+        return userId;
     }
 
     public long getJourneyId() {
-        return journey_id;
+        return journeyId;
     }
 
     public String getMessage() {
