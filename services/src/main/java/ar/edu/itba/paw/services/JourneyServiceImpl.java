95b521ee517f9a8d8058bba9a0d785793e47532e
mrossisei <mrossiseifert@itba.edu.ar>
2025-03-30 10:51:47 -0300

    Implement replyToJourney in JourneyService

--- services/src/main/java/ar/edu/itba/paw/services/JourneyServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/JourneyServiceImpl.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.persistence.JourneyDao;
+import ar.edu.itba.paw.interfaces.persistence.JourneyResponseDao;
 import ar.edu.itba.paw.interfaces.services.JourneyService;
 import ar.edu.itba.paw.interfaces.services.UniversityService;
 import ar.edu.itba.paw.interfaces.services.UserService;
@@ -21,12 +22,14 @@ public class JourneyServiceImpl implements JourneyService {
     private final JourneyDao journeyDao;
     private final UserService userService;
     private final UniversityService universityService;
+    private final JourneyResponseDao journeyResponseDao;
 
     @Autowired
-    public JourneyServiceImpl(JourneyDao journeyDao, UserService userService, UniversityService universityService) {
+    public JourneyServiceImpl(JourneyDao journeyDao, UserService userService, UniversityService universityService, JourneyResponseDao journeyResponseDao) {
         this.journeyDao = journeyDao;
         this.userService = userService;
         this.universityService = universityService;
+        this.journeyResponseDao = journeyResponseDao;
     }
 
 
@@ -39,22 +42,29 @@ public class JourneyServiceImpl implements JourneyService {
         user = maybeUser.orElseGet(() -> userService.createUser(email, username, firstname, lastname, originUniversity, career, profilePictureId)); // todo: ¿acá debería usar el service o el dao? -> el service puede llegar a tener validaciones que ya acabo de hacer en esta clase
         // id me lo da la bd btw
         // return journeyDao.create(user, destinationCity, startDate, endDate, destination, description);
-        // FIXME: chequear si el journey ya existe (¿solo si no cree el usuario?)
+        // FIXME: chequear si el journey ya existe? (¿solo si no cree el usuario?) -> chequear sino que las fechas no se solapen
         return journeyDao.create(user, destination, destinationCity, startDate, endDate, description);
     }
 
     @Override
     public void replyToJourney(String email, String username, String firstname, String lastname, String originUniversity, String career, long profilePictureId, long journeyId, String message) {
-        // comprobar si existe el usuario
-        // Optional<User> user = userService.findByEmail(email);
-        // crearlo si no existe
-        // if (!user.isPresent()) {
-        //     User newUser = userService.createUser(email, username, firstname, lastname, originUniversity, career, profilePictureId);
-        // }
-        // buscar el journey por id
-        // Optional<Journey> journey = journeyDao.findById(journeyId);
-        // si no existe, manejar el error -> ¿? -> throw new RuntimeException("Journey not found"); o NotFoundException o JourneyNotFoundException o algo así?
+        journeyDao.findById(journeyId).orElseThrow(() -> new RuntimeException("Journey not found")).getId();
+        /*
+        Optional<Journey> maybeJourney = journeyDao.findById(journeyId);
+        if (maybeJourney.isEmpty()) {
+            throw new RuntimeException("Journey not found");
+        }
+        */
+        long userId = userService.findByEmail(email).orElseGet(() -> userService.createUser(email, username, firstname, lastname, originUniversity, career, profilePictureId)).getId();
+        // tal vez deberíamos chequear por username también -> si intenta repetirlo nos va a caer una excepción de la bd
+        /*
+        Optional<User> maybeUser = userService.findByEmail(email);
+        User user = maybeUser.orElseGet(() -> userService.createUser(email, username, firstname, lastname, originUniversity, career, profilePictureId));
+        */
+
         // mandar el mail
+
+        journeyResponseDao.create(userId, journeyId, message);
 
     }
 
