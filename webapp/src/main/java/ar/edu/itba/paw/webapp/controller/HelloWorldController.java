d344472970bbd22624057d8ea30ea7800b41d426
lulardou <lulostupnik2003@gmail.com>
2025-04-03 14:53:05 -0300

    msg fix

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -27,31 +27,5 @@ public class HelloWorldController {
         return mav;
     }
 
-    @GetMapping("/sendEmail")
-    public String sendEmail() {
-
-        // Set Spanish as the user's locale
-        Locale userLocale = new Locale("es");
-
-        // 1) Prepare the subject arguments, for the key: "mail.subject.welcome"
-        //    which includes a placeholder {0}.
-        Object[] subjectArgs = {"Carlos"};
-
-        // 2) Variables for the Thymeleaf template (the HTML body)
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("name", "Carlos");
-
-        // 3) Send the email
-        emailService.sendHtmlMessage(
-                "lstupnik@itba.edu.ar",    // Destination email address
-                "mail.subject.welcome",     // The subject key from messages_es.properties
-                subjectArgs,                // Replacement for placeholders (i.e. {0} -> "Carlos")
-                "welcome",          // The Thymeleaf template name (no .html if configured)
-                variables,                  // Variables map for the template
-                userLocale                  // <--- Spanish
-        );
-        // Return a simple status
-        return "Email sent in Spanish!"; //esto falla lo q vuelve, pero no importa es para probar. 
-    }
 
 }
