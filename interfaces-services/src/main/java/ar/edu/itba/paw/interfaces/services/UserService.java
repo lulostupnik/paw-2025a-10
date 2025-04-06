8d38c2a6b2ad0faadd00bc07c19311fdf9b5f09b
mrossisei <mrossiseifert@itba.edu.ar>
2025-04-06 12:49:54 -0300

    Update UserService

--- interfaces-services/src/main/java/ar/edu/itba/paw/interfaces/services/UserService.java
+++ interfaces-services/src/main/java/ar/edu/itba/paw/interfaces/services/UserService.java
@@ -5,6 +5,11 @@ import ar.edu.itba.paw.models.User;
 import java.util.Optional;
 
 public interface UserService {
+    // dejar de usar esta
     User createUser(String email, String username, String firstname, String lastname, String universityName, String career, long profilePictureId);
+
+    // usar esta
+    User createUser(String email, String username, String firstname, String lastname, String universityName, String career, byte[] profilePicture);
+
     Optional<User> findByEmail(String email);
 }
--- services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.persistence.ImageDao;
 import ar.edu.itba.paw.interfaces.persistence.UserDao;
 import ar.edu.itba.paw.interfaces.services.UniversityService;
 import ar.edu.itba.paw.interfaces.services.UserService;
@@ -8,6 +9,7 @@ import ar.edu.itba.paw.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -15,22 +17,31 @@ public class UserServiceImpl implements UserService {
 
     private final UniversityService universityService;
     private final UserDao userDao;
+    private final ImageDao imageDao;
 
     @Autowired
-    public UserServiceImpl(UniversityService universityService, UserDao userDao) {
+    public UserServiceImpl(UniversityService universityService, UserDao userDao, ImageDao imageDao) {
         this.universityService = universityService;
         this.userDao = userDao;
+        this.imageDao = imageDao;
     }
 
     public User createUser(String email, String username, String firstname, String lastname, String universityName, String career, long profilePictureId) {
-        Optional<University> university = universityService.findByName(universityName);
-        if (university.isEmpty()) {
-            throw new RuntimeException("University not found");
-        }
+        University university = universityService.findByName(universityName).orElseThrow(() -> new RuntimeException("University not found"));
+
         // subir imagen ¿puede ser async? -> supongo que no porque necesito el id de la imagen para crear el usuario
         // todo: ¿chequear si el usuario ya existe? -> ¿o dejo que la excepción la tire la capa de persistencia?
-        return userDao.create(email, username, firstname, lastname, university.get(), career, profilePictureId);
+        return userDao.create(email, username, firstname, lastname, university, career, profilePictureId);
     }
+
+    // creo que debería ser @Transactional
+    @Override
+    public User createUser(String email, String username, String firstname, String lastname, String universityName, String career, byte[] profilePicture) {
+        University university = universityService.findByName(universityName).orElseThrow(() -> new RuntimeException("University not found"));
+        long profilePictureId = imageDao.saveImage(profilePicture);
+        return userDao.create(email, username, firstname, lastname, university, career, profilePictureId);
+    }
+
 
     public Optional<User> findByEmail(String email) {
         return userDao.findByEmail(email);
