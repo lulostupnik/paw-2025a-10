884f9c1a292405d23c156dae38d169b2c479ceca
mrossisei <mrossiseifert@itba.edu.ar>
2025-04-06 12:28:11 -0300

    Fix CityDao

--- interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/CityDao.java
+++ interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/CityDao.java
@@ -10,5 +10,6 @@ public interface CityDao {
     List<City> findAll();
     List<City> findAllByCountry(String country);
     List<City> findAllBySubstring(String substring); // method to find all the cities which have substring in their name
+    Optional<City> findByName(String name); // method to find a city by its name
     // add method that finds cities with a "similar" name -> using Levensthein?
 }
