8ea9c1e6fee8e2d3e03e1bc75dea6f63216aacbe
lulardou <lulostupnik2003@gmail.com>
2025-04-07 23:39:45 -0300

    idioma config

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/CustomLocaleResolver.java
@@ -0,0 +1,52 @@
+//
+//
+//
+//
+//package ar.edu.itba.paw.webapp.config;
+//
+//import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.*;
+//
+//public class CustomLocaleResolver extends AcceptHeaderLocaleResolver {
+//
+//    private static final Locale EN = new Locale("en");
+//    private static final Locale ES = new Locale("es");
+//    private static final List<Locale> SUPPORTED_LOCALES = Arrays.asList(EN, ES);
+//    private static final Locale DEFAULT_LOCALE = EN;
+//
+//    @Override
+//    public Locale resolveLocale(HttpServletRequest request) {
+//        Locale requestLocale = super.resolveLocale(request);
+//        return resolveSupportedOrDefault(requestLocale);
+//    }
+//
+//    @Override
+//    public void setLocale(HttpServletRequest request,
+//                          HttpServletResponse response,
+//                          Locale locale) {
+//        setDefaultLocale(resolveSupportedOrDefault(locale));
+//    }
+//
+//    /**
+//     * Returns the supported locale (en or es) if matched, otherwise returns the default.
+//     */
+//    private Locale resolveSupportedOrDefault(Locale locale) {
+//        if (locale == null) {
+//            return DEFAULT_LOCALE;
+//        }
+//
+//        String language = locale.getLanguage().toLowerCase();
+//
+//        for (Locale supported : SUPPORTED_LOCALES) {
+//            if (supported.getLanguage().equals(language)) {
+//                return supported;
+//            }
+//        }
+//
+//        return DEFAULT_LOCALE;
+//    }
+//}
+//
--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -67,6 +68,7 @@ public class WebConfig /* extends WebMvcConfigurerAdapter */ {
         ms.setCacheSeconds((int) TimeUnit.MINUTES.toSeconds(5));
         ms.setBasename("classpath:i18n/messages");
         ms.setDefaultEncoding(StandardCharsets.UTF_8.name());
+
         return ms;
     }
 
@@ -122,14 +124,11 @@ public class WebConfig /* extends WebMvcConfigurerAdapter */ {
         templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
         templateResolver.setCacheable(false); // Disable cache for development
         return templateResolver;
-
-    /*
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        super.addResourceHandlers(registry);
-        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
     }
-    */
 
-}
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        return new CustomLocaleResolver();
+//    }
+
 }
