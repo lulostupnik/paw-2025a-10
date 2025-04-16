e0df4fabe7f332b971b71b9538e7626f17274b5c
mrossisei <mrossiseifert@itba.edu.ar>
2025-04-16 09:31:44 -0300

    Update events_attendances table

--- webapp/src/main/resources/sql/schema.sql
+++ webapp/src/main/resources/sql/schema.sql
@@ -134,7 +134,7 @@ CREATE TABLE IF NOT EXISTS event_responses (
     PRIMARY KEY (user_id, event_id)
     );
 
-CREATE TABLE IF NOT EXISTS events_attendances (
+CREATE TABLE IF NOT EXISTS event_attendances (
         user_id INTEGER NOT NULL,
         event_id INTEGER NOT NULL,
 
