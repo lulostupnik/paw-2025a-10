df68a8ad7e00beda3794e87e019aed332e379789
lulardou <lulostupnik2003@gmail.com>
2025-04-16 22:59:05 -0300

    fixed merge

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/EventController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/EventController.java
@@ -104,14 +104,24 @@ public class EventController {
     @RequestMapping("/{id}")
     public ModelAndView getEvent(@PathVariable long id) {
         LOGGER.debug("Getting info for event {}", id);
-        Optional<Event> event = eventService.getEventById(id);
-        if (event.isEmpty()) {
+        Optional<Event> maybeEvent = eventService.getEventById(id);
+        if (maybeEvent.isEmpty()) {
             LOGGER.warn("Event {} not found", id);
             return new ModelAndView("events/not_found");
         }
-        LOGGER.info("Found event {}", event.get());
+
+        Event event = maybeEvent.get();
+        LOGGER.info("Found event {}", event);
+
+        List<EventResponse> eventResponses = eventService.getEventResponses(event.getId());
+
+        Boolean attend = eventService.isUserAttending(SecurityContextHolder.getContext().getAuthentication().getName(), id);
+
         ModelAndView mav = new ModelAndView("events/detail");
-        mav.addObject("event", event.get());
+        mav.addObject("event", event);
+        mav.addObject("attendees", eventService.getEventAttendees(id));
+        mav.addObject("attend", attend);
+        mav.addObject("eventResponses", eventResponses);
         return mav;
     }
 
@@ -152,5 +162,13 @@ public class EventController {
         return new ModelAndView("redirect:/events");
 
     }
+
+    @RequestMapping(value="/{id}/attend",method = POST,produces = "application/json")
+   public ModelAndView attendEvent(@PathVariable int id) {
+                LOGGER.debug("Attending event {}", id);
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                eventService.attendEvent(authentication.getName(), id);
+                return new ModelAndView("redirect:/events/{id}");
+            }
     
 }
