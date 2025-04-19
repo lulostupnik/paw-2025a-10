3d62c8044203d76b0d6085cc2fd3a2913dbd3901
ivovila <ivilamowski@itba.edu.ar>
2025-04-19 18:50:56 -0300

    Final changes

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -86,8 +86,8 @@ public class WebConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
         ds.setDriverClass(org.postgresql.Driver.class);
-        ds.setUrl("jdbc:postgresql://localhost/paw");
-        ds.setUsername("postgres");
+        ds.setUrl("jdbc:postgresql://localhost/paw-2025a-10");
+        ds.setUsername("paw-2025a-10");
         ds.setPassword("<CONFIGURAR_LOCALMENTE>");
         return ds;
     }
@@ -116,7 +116,7 @@ public class WebConfig implements WebMvcConfigurer {
         dbp.addScript(schemaSql);
         return dbp;
     }
-        @Bean
+    @Bean
     public StandardServletMultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
     }
