0de3e7dd1239001e3d69c3864b270cbcc909e22c
mrossisei <mrossiseifert@itba.edu.ar>
2025-05-01 15:47:49 -0300

    Update UserPassword to contain only the concerning fields

--- persistence/src/test/java/ar/edu/itba/paw/persistance/UserJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistance/UserJdbcDaoTest.java
@@ -225,7 +225,7 @@ public class UserJdbcDaoTest {
         assertTrue(maybeUser.isPresent());
         final UserPassword user = maybeUser.get();
         assertEquals(PASSWORD, user.getPassword());
-        assertEqualsUser(user);
+        // assertEqualsUser(user);
     }
     @Test
     public void testFindUserByEmailWithPasswordMissing(){
