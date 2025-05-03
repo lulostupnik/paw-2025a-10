0ff58b4d97003597fff52d8ee1188a2b228a8ea6
mrossisei <mrossiseifert@itba.edu.ar>
2025-05-03 16:09:52 -0300

    Clean UniversityJdbcDao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UniversityJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UniversityJdbcDao.java
@@ -61,57 +61,58 @@ public class UniversityJdbcDao implements UniversityDao {
 
     @Override
     public Optional<University> findByName(String name) {
-        LOGGER.debug("Querying DB for university name {}", name);
         return jdbcTemplate.query(QUERY + " AND un.name = ?", UNIVERSITY_ROW_MAPPER, name).stream().findFirst();
     }
 
     @Override
     public Optional<University> findByAbbreviation(String abbreviation) {
-        LOGGER.debug("Querying DB for university abbreviation {}", abbreviation);
         return jdbcTemplate.query(QUERY + " AND un.abbreviation = ?", UNIVERSITY_ROW_MAPPER, abbreviation).stream().findFirst();
     }    
     
     @Override
     public Optional<University> findByAny(String searchString) {
-        LOGGER.debug("Querying DB for university like {}", searchString);
         return jdbcTemplate.query(QUERY + " AND (un.abbreviation LIKE ? OR un.name LIKE ?)", UNIVERSITY_ROW_MAPPER, "%"+searchString+"%", "%"+searchString+"%").stream().findFirst();
     }
 
     @Override
     public List<University> getAllUniversities() {
-        LOGGER.debug("Querying DB for all universities");
         return jdbcTemplate.query(QUERY + " ORDER BY un.name", UNIVERSITY_ROW_MAPPER);
     }
 
     @Override
     public Page<University> searchBySubstring(String substring, int page, int size) {
         final String like = "%" + substring + "%";
-        int offset = (page - 1) * size;
 
         final String sql = QUERY + " AND (LOWER(un.name) LIKE LOWER(?) OR LOWER(un.abbreviation) LIKE LOWER(?)) LIMIT ? OFFSET ?";
-        int totalUniversities = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM universities WHERE deleted = FALSE AND (LOWER(name) LIKE LOWER(?) OR LOWER(abbreviation) LIKE LOWER(?))", Integer.class, like, like);
-        int totaPages = (int) Math.ceil((double) totalUniversities / size);
-        return new Page<>(jdbcTemplate.query(sql, UNIVERSITY_ROW_MAPPER, like, like, size, offset), page, totaPages);
+
+        int totalItems = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM universities WHERE deleted = FALSE AND (LOWER(name) LIKE LOWER(?) OR LOWER(abbreviation) LIKE LOWER(?))", Integer.class, like, like);
+        return new Page<>(
+                jdbcTemplate.query(sql, UNIVERSITY_ROW_MAPPER, like, like, size, (page - 1) * size),
+                page,
+                (int) Math.ceil((double) totalItems / size)
+        );
     }
 
 
 
     @Override
     public Optional<University> findById(long id) {
-        LOGGER.debug("Querying DB for university with id {}", id);
         return jdbcTemplate.query(QUERY + " AND un.id = ?", UNIVERSITY_ROW_MAPPER, id).stream().findFirst();
     }
 
     @Override
     public Page<University> getAllUniversities(int page, int size) {
-        int offset = (page - 1) * size;
         StringBuilder query = new StringBuilder(SELECT_CLAUSE);
         query.append(" FROM (SELECT * FROM universities WHERE deleted = FALSE LIMIT ? OFFSET ?) un")
                 .append(" JOIN cities ci ON un.city_id = ci.id")
                 .append(" JOIN countries co ON ci.country_id = co.id");
-        int totalUniversities = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM universities WHERE deleted = FALSE", Integer.class);
-        int totalPages = (int) Math.ceil((double) totalUniversities / size);
-        return new Page<>(jdbcTemplate.query(query.toString(), UNIVERSITY_ROW_MAPPER, size, offset),page,totalPages);
+        int totalItems = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM universities WHERE deleted = FALSE", Integer.class);
+
+        return new Page<>(
+                jdbcTemplate.query(query.toString(), UNIVERSITY_ROW_MAPPER, size, (page - 1) * size),
+                page,
+                (int) Math.ceil((double) totalItems / size)
+        );
     }
 
     //FIXME: consultar con los profes -> ¿debería recibir City city o String city? ¿O que?
