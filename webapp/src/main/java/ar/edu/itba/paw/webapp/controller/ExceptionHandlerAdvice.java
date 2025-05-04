309df17a8bbd3d20311b7c803b47f7bdcbdd68c3
lulardou <lulostupnik2003@gmail.com>
2025-05-04 14:05:09 -0300

    added logging to exception hanldler

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionHandlerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionHandlerAdvice.java
@@ -13,24 +13,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ModelAndView error404() {
+    public ModelAndView error404(NotFoundException ex) {
+        LOGGER.warn("NotFoundException: {}", ex.toString());
+        LOGGER.debug("Stack trace for NotFoundException", ex);
+
         ModelAndView mav = new ModelAndView("errors/error");
         mav.addObject("errorType", "404");
-//        mav.addObject("errorIcon", "shield-off");
         return mav;
     }
 
     @ExceptionHandler(InvalidException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ModelAndView error400() {
+    public ModelAndView error400(InvalidException ex) {
+        LOGGER.warn("InvalidException: {}", ex.getMessage());
+        LOGGER.debug("Stack trace for InvalidException", ex);
+
         ModelAndView mav = new ModelAndView("errors/error");
         mav.addObject("errorType", "400");
         return mav;
@@ -38,20 +42,28 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(EventNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ModelAndView error404Event() {
+    public ModelAndView error404Event(EventNotFoundException ex) {
+        LOGGER.warn("EventNotFoundException: {}", ex.getMessage());
+        LOGGER.debug("Stack trace for EventNotFoundException", ex);
+
         return new ModelAndView("events/not_found");
     }
 
     @ExceptionHandler(JourneyNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ModelAndView error404Journey() {
+    public ModelAndView error404Journey(JourneyNotFoundException ex) {
+        LOGGER.warn("JourneyNotFoundException: {}", ex.getMessage());
+        LOGGER.debug("Stack trace for JourneyNotFoundException", ex);
+
         return new ModelAndView("journeys/not_found");
     }
 
-
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(code = HttpStatus.FORBIDDEN)
-    public ModelAndView error403() {
+    public ModelAndView error403(AccessDeniedException ex) {
+        LOGGER.warn("AccessDeniedException: {}", ex.getMessage());
+        LOGGER.debug("Stack trace for AccessDeniedException", ex);
+
         ModelAndView mav = new ModelAndView("errors/error");
         mav.addObject("errorType", "403");
         mav.addObject("errorIcon", "shield-off");
@@ -60,10 +72,12 @@ public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView error500() {
+    public ModelAndView error500(Exception ex) {
+        LOGGER.warn("Unhandled exception: {}", ex.toString());
+        LOGGER.debug("Stack trace for general Exception", ex);
+
         ModelAndView mav = new ModelAndView("errors/error");
         mav.addObject("errorType", "500");
-//        mav.addObject("errorIcon", "shield-off");
         return mav;
     }
 }
