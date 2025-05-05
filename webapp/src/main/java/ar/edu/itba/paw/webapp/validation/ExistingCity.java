0d15480149b59e5ed873db701fcc17191e1e93d5
Nicolas Koron <nkoron@itba.edu.ar>
2025-05-05 00:54:05 +0000

    bug

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validation/ExistingCity.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validation/ExistingCity.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = {EmailNotInUseValidator.class})
+@Constraint(validatedBy = {CityNameValidator.class})
 public @interface ExistingCity {
     String message() default "{EmailNotInUse.message}";
 
