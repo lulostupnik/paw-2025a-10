122f3570b7240ac2262ed48f32023241b816ede9
lulostupnik <lulostupnik2003@gmail.com>
2025-05-10 19:58:06 -0300

    deleted unused model

--- interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/CountryDao.java
+++ interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/CountryDao.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.paw.interfaces.persistence;
 
 import ar.edu.itba.paw.models.Country;
-import ar.edu.itba.paw.models.CursorPage;
 
 import java.util.List;
 import java.util.Optional;
