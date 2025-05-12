50a5add1ad0803eacc659441d1f79478990e3296
mrossisei <mrossiseifert@itba.edu.ar>
2025-05-11 23:13:23 -0300

    Clean

--- services/src/main/java/ar/edu/itba/paw/services/EventServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EventServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
