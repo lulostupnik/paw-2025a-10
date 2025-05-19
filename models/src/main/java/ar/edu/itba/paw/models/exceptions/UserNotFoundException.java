e07d792b1fabafe9ed366565481bbfa9567e23ef
mrossisei <mrossiseifert@itba.edu.ar>
2025-05-19 12:36:17 -0300

    Adjusting services and daos

---
+++ models/src/main/java/ar/edu/itba/paw/models/exceptions/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.paw.models.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+    public UserNotFoundException(){
+        super("Journey not found");
+    }
+}
