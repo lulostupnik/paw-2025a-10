9a5a510169a641d35aac3f39f6728c1249908c52
lulostupnik <lulostupnik2003@gmail.com>
2025-06-13 16:18:21 -0300

    forgot to delete fixme

--- persistence/src/main/java/ar/edu/itba/paw/persistence/JourneyHibernateDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/JourneyHibernateDao.java
@@ -350,7 +350,7 @@ public class JourneyHibernateDao implements JourneyDao {
             origin_uni_match_off_travel_score +
             origin_city_match_off_travel_score
         ) DESC
-    """; //fixme ES POSIBLE QUE HAYAN JOINS innecesarios porque estaban por el ROWMAPPER.
+    """; 
 
         final String jpqlFetch = "FROM Journey j WHERE j.id IN :ids";
 
