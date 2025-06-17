5093664dd70bd1c455d382b1ce043f3ceca934a5
Nicolas Koron <nkoron@itba.edu.ar>
2025-06-17 00:41:23 +0000

    new bad request exceptions extend InvalidException

--- models/src/main/java/ar/edu/itba/paw/models/exceptions/EventNotInTheFutureException.java
+++ models/src/main/java/ar/edu/itba/paw/models/exceptions/EventNotInTheFutureException.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.models.exceptions;
 
-public class EventNotInTheFutureException extends RuntimeException {
+public class EventNotInTheFutureException extends InvalidException {
     public EventNotInTheFutureException(long id) {
         super(String.format("Event with id %d is not in the future", id));
     }
--- models/src/main/java/ar/edu/itba/paw/models/exceptions/UserAlreadyAttendingException.java
+++ models/src/main/java/ar/edu/itba/paw/models/exceptions/UserAlreadyAttendingException.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.models.exceptions;
 
-public class UserAlreadyAttendingException extends RuntimeException {
+public class UserAlreadyAttendingException extends InvalidException {
     public UserAlreadyAttendingException(long id, long eventId) {
         super(String.format("User %d is already attending event with id %d", id, eventId));
     }
