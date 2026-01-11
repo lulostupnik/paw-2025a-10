d672d61b0c1e7d52c37b956562f81b5be6b08d03
mrossisei <mrossiseifert@itba.edu.ar>
2026-01-11 12:48:14 -0300

    Added methods to EventRatingDao

--- interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/EventRatingDao.java
+++ interfaces-persistence/src/main/java/ar/edu/itba/paw/interfaces/persistence/EventRatingDao.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.interfaces.persistence;
 
+import ar.edu.itba.paw.models.Page;
+import ar.edu.itba.paw.models.PageParams;
 import ar.edu.itba.paw.models.Rating;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.Event;
@@ -9,6 +11,12 @@ public interface EventRatingDao {
     Rating rateEvent(User user, Event event, double rating);
 
     Optional<Rating> findRatingByUserAndEvent(long userId, long eventId);
+
+    Optional<Rating> findById(long ratingId);
+
+    Page<Rating> findByEventId(long eventId, PageParams pageParams);
+
+    void delete(long ratingId);
 
     int countRatingsByEvent(long eventId);
 
