321ed5256bc77591ca0d24288af8e4fcccad5f9d
Tomas Gonzalez Colasantti <tgonzalezcolasantti@itba.edu.ar>
2026-01-26 17:04:39 -0300

    fix backend bugs

--- webapp/src/main/java/ar/edu/itba/paw/webapp/utils/PagingUtils.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/utils/PagingUtils.java
@@ -4,12 +4,18 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ar.edu.itba.paw.models.Page;
 
 public class PagingUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PagingUtils.class);
+
     public static <T> ResponseBuilder insertPaginationLinks(ResponseBuilder responseBuilder, UriInfo uriInfo, Page<T> page){
         UriBuilder uriBuilder = uriInfo.getRequestUriBuilder().replaceQueryParam("page", "{page}");
-        if (page.getTotalPages() < page.getCurrentPage())
+        LOGGER.warn("" + page.getTotalPages() + page.getCurrentPage());
+        if (page.getTotalPages() > page.getCurrentPage())
             responseBuilder.link(uriBuilder.build( page.getCurrentPage() + 1), "next");
         if (page.getCurrentPage() > 1)
             responseBuilder.link(uriBuilder.build( page.getCurrentPage() - 1), "prev");
