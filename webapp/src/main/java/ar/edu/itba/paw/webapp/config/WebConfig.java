0f595b3cc3f281e047903245a9d26dcb1240ba03
mrossisei <mrossiseifert@itba.edu.ar>
2026-07-21 23:04:00 -0300

    chore(deploy)

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -100,7 +100,7 @@ public class WebConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false");
         return mailSender;
     }
 
